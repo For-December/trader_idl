873b1bddb6a7683e8a2c871755569b91e576969f
fordecember <1921567337@qq.com>
2025-06-16 01:18:41 +0800

    fix: 移除旧产物

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
-cd gen_go/scaping_go
-kitex --module github.com/For-December/trader_idl/gen_go/scaping_go -service caping_service  ../../idl/scaping_go/service.thrift 
+cd gen_go/scraping_go
+kitex --module github.com/For-December/trader_idl/gen_go/scraping_go -service caping_service  ../../idl/scraping_go/service.thrift 
 
 cd gen-py/brain_py
-thriftgo -r -out . --gen py ../../idl/scaping_go/service.thrift
+thriftgo -r -out . --gen py ../../idl/scraping_go/service.thrift
+
+thrift --gen py -out . ../../idl/scraping_go/service.thrift
--- gen_go/scaping_go/go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/For-December/trader_idl/gen_go/scaping_go
-
-go 1.24.4
-
-replace github.com/apache/thrift => github.com/apache/thrift v0.13.0
