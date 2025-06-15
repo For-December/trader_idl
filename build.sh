b8d95d52a40eccd60430ea8156ac9b02b8df25dd
fordecember <1921567337@qq.com>
2025-06-16 01:20:43 +0800

    feat: 更新产物

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 cd gen_go/scraping_go
-kitex --module github.com/For-December/trader_idl/gen_go/scraping_go -service caping_service  ../../idl/scraping_go/service.thrift 
+kitex --module github.com/For-December/trader_idl/gen_go/scraping_go -service scraping_service  ../../idl/scraping_go/service.thrift 
 
 cd gen-py/brain_py
 thriftgo -r -out . --gen py ../../idl/scraping_go/service.thrift
--- gen_go/scraping_go/build.sh
+++ gen_go/scraping_go/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-RUN_NAME="caping_service"
+RUN_NAME="scraping_service"
 
 mkdir -p output/bin
 cp script/* output/
